dd8ca38ad77b33d203cdbd0fbb9b2270ba1f9874
Mfix83 <123844738+Mfix83@users.noreply.github.com>
2023-05-13 20:31:50 -0400

    V2 Changes

--- src/com/coderscampus/java2/JavaAssignment2.java
+++ src/com/coderscampus/java2/JavaAssignment2.java
@@ -4,7 +4,14 @@ public class JavaAssignment2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//guessing game
+		
+		//generate a random number
+		
+		//collect user input
 
+		//player gets 5 turns
 	}
 
 }
