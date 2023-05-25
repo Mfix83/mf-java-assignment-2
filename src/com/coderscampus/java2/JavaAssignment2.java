cb67765c901f0838876ec70dd3da7c384e258d99
Mfix83 <123844738+Mfix83@users.noreply.github.com>
2023-05-25 16:27:28 -0400

    Update JavaAssignment2.java

--- src/com/coderscampus/java2/JavaAssignment2.java
+++ src/com/coderscampus/java2/JavaAssignment2.java
@@ -1,8 +1,46 @@
 package com.coderscampus.java2;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class JavaAssignment2 {
 
 	public static void main(String[] args) {
+		
+		Random random = new Random();
+	    int randomNumber = random.nextInt(100) + 1;
+        int maxGuesses = 5;
+        int numGuesses = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+		System.out.println("Choose a number between 1 and 100. You will have 5 guesses!");
+				while (numGuesses < maxGuesses) {
+		            System.out.print("Choose a random number between 1 and 100: ");
+		            int guess = scanner.nextInt();
+		            
+		            if (guess < 1 || guess > 100) {
+		                System.out.println("Your guess is not between 1 and 100, please try again.");
+		                continue;
+		            }
+		            
+		            numGuesses++;
+		
+		            if (guess < randomNumber) {
+		                System.out.println("Please pick a higher number.");
+		            } else if (guess > randomNumber) {
+		                System.out.println("Please pick a lower number.");
+		            } else {
+		                System.out.println("You win! Game over!");
+		                return; // Game over
+		            }
+		        }
+		        
+		        System.out.println("You lose, the number to guess was " + randomNumber);
+		        scanner.close();
+		    }
+		
+		//if statement...boolean for True/False...While loop?? end condition will be 5 attempts??
 		// TODO Auto-generated method stub
 		
 		//guessing game
@@ -14,4 +52,6 @@ public class JavaAssignment2 {
 		//player gets 5 turns
 	}
 
-}
+
+
+	
