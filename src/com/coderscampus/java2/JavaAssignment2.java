fae1ce60c70c9356cb52c1a792bab4cb35c985f8
Mfix83 <123844738+Mfix83@users.noreply.github.com>
2023-06-21 15:26:31 -0400

    Updated

--- src/com/coderscampus/java2/JavaAssignment2.java
+++ src/com/coderscampus/java2/JavaAssignment2.java
@@ -18,27 +18,36 @@ public class JavaAssignment2 {
 
         while (numGuesses < maxGuesses) {
             System.out.print("Choose a random number between 1 and 100: ");
-            int guess = scanner.nextInt();
-            numGuesses++;
+            
+            if (scanner.hasNextInt()) {
+                int guess = scanner.nextInt();
+                scanner.nextLine();
+                
+                if (guess < 1 || guess > 100) {
+                    System.out.println("Your guess is not between 1 and 100, please try again.");
+                    continue;
+                }
 
-            if (guess < 1 || guess > 100) {
-                System.out.println("Your guess is not between 1 and 100, please try again.");
-                continue;
-            }
+                numGuesses++;
 
-            if (guess < randomNumber) {
-                System.out.println("Please pick a higher number.");
-            } else if (guess > randomNumber) {
-                System.out.println("Please pick a lower number.");
+                if (guess < randomNumber) {
+                    System.out.println("Please pick a higher number.");
+                } else if (guess > randomNumber) {
+                    System.out.println("Please pick a lower number.");
+                } else {
+                    System.out.println("You win! Game over!");
+                    scanner.close(); // Close the scanner
+                    return; // Game over
+                }
             } else {
-                System.out.println("You win! Game over!");
-                scanner.close();
-                return; // Game over
+                scanner.nextLine(); // Clear the invalid input from the scanner
             }
         }
 
         System.out.println("You lose, the number to guess was " + randomNumber);
-        scanner.close();
+        scanner.close(); // Close the scanner
     }
-
 }
+
+
+
